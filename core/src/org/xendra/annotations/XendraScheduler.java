7e7f654a28f86c1deeab98a7c474455a323a733e
xapiens <fmorosini@dravios.com>
2022-05-18 20:26:41 -0500

    added annotation to add to scheduler

---
+++ core/src/org/xendra/annotations/XendraScheduler.java
@@ -0,0 +1,16 @@
+package org.xendra.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(value = ElementType.TYPE)
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface XendraScheduler {
+	String frequencytype();
+	String frequency();
+	String keeplogdays();
+	String name();
+	String type();
+}
