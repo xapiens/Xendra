ab80ea21963d10057c7abbfd238edec47e3bc325
xapiens <fmorosini@dravios.com>
2023-03-05 18:17:42 -0500

    moved certificate security to own plugin

--- core/src/org/xendra/security/gui/frame/SecurityEditorFrameMediator.java
+++
@@ -1,9 +0,0 @@
-package org.xendra.security.gui.frame;
-
-
-import org.columba.api.gui.frame.IFrameMediator;
-import org.columba.core.gui.action.AbstractColumbaAction;
-
-public interface SecurityEditorFrameMediator extends IFrameMediator {
-	//public SecurityPanel getSecurityPanel();
-}
